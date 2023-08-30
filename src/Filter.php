3e7cd376770ce8b3e34c3131d4f02a999d62ce02
Ibnul Mutaki <ibnuul@gmail.com>
2023-08-30 21:08:25 +0700

    prepare for Filter class

---
+++ src/Filter.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Cacing69\Cquery;
+use Cacing69\Cquery\Trait\HasNodeProperty;
+use Cacing69\Cquery\Trait\HasValueProperty;
+use Cacing69\Cquery\Trait\HasOperatorProperty;
+
+class Filter
+{
+    use HasNodeProperty;
+    use HasOperatorProperty;
+    use HasValueProperty;
+}
---
+++ src/Trait/HasValueProperty.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Cacing69\Cquery\Trait;
+
+trait HasValueProperty {
+    private $value;
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}

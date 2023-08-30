2294d33fa2800faa50c3161d99ef8855ecee7055
Ibnul Mutaki <ibnuul@gmail.com>
2023-08-30 22:33:45 +0700

    prepare for for replace adapter

---
+++ src/Adapter/ReplaceCallbackAdapter.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+namespace Cacing69\Cquery\Adapter;
+
+use Cacing69\Cquery\Extractor\SourceExtractor;
+use Cacing69\Cquery\Support\RegExp;
+
+class ReplaceCallbackAdapter extends CallbackAdapter
+{
+    protected static $signature = '/^\s*replace\(\s*(.*?)\s*,\s*(.*?),\s*(.*?)\)\s*/';
+
+    public static function getSignature() {
+        return self::$signature;
+    }
+
+    public function __construct(string $raw, SourceExtractor $source = null)
+    {
+        $this->raw = $raw;
+
+        preg_match(RegExp::EXTRACT_FIRST_PARAM_ATTRIBUTE, $raw, $attr);
+        preg_match(RegExp::EXTRACT_SECOND_PARAM_ATTRIBUTE, $raw, $node);
+
+        $this->ref = $attr[1];
+        $this->node = $node[1];
+
+        $this->call = "extract";
+        $this->callParameter = [$this->ref];
+    }
+}
